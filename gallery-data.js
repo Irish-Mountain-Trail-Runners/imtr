66e25ad5f23f3a8941a20f1c53ed4fc4000a524b
Irish-Mountain-Trail-Runners <imtr.michael@gmail.com>
2026-06-22 23:54:11 +0100

    Add files via upload

--- gallery-data.js
+++ gallery-data.js
@@ -2,19 +2,19 @@ const galleries = [
 
     {
         title: "Saturday Run - 20 June 2026",
-        folder: "frame01",
+        folder: "images/gallery/frame01",
         images: 25
     },
 
     {
         title: "Saturday Run - 20 June 2026 - 2",
-        folder: "frame02",
+        folder: "images/gallery/frame02",
         images: 25
     },
 
     {
         title: "Saturday Run - 20 June 2026 - 3",
-        folder: "frame03",
+        folder: "images/gallery/frame03",
         images: 25
     }
 
