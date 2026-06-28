843f06876a238e3b4da3552ee5c121dab172e13c
Irish-Mountain-Trail-Runners <imtr.michael@gmail.com>
2026-06-28 23:26:03 +0100

    Add files via upload

--- gallery-data.js
+++ gallery-data.js
@@ -1,5 +1,10 @@
 const galleries = [
     {
+        title: "27 Jun 26 - Club Run",
+        folder: "frame04",
+        images: 57
+    },
+    {
         title: "IMTR Midweek Run (Puerto Del Cruagh) - 24 June 2026",
         folder: "frame01",
         images: 20   // change this number to how many images you actually have
