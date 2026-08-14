b1130c54a4eea0870aa44bd62b714a0af7f39720
Irish Mountain Trail Runners <imtr.michael@gmail.com>
2026-08-14 12:40:50 +0100

    Add gallery: Wednesday 5th August, Masseys 6k

--- gallery-data.js
+++ gallery-data.js
@@ -1,5 +1,11 @@
 const galleries = [
     {
+      title: "Wednesday 5th August, Masseys 6k",
+      description: "Cracking evening on the Masseys trails for our 6k run, we managed to avoid the showers which was a bonus 😁. Everyone looking really strong and as usual the chats were 👌 Super running by everyone. Well done gang. #irishmountaintrailrunners #IMTR #clubrun #runchats #consistencyiskey",
+      folder: "5th August",
+      images: 20,
+    },
+    {
       title: "Sat 1st August 4k,7k,10k Cruagh/Glendoo",
       description: "Starting off the Bank Holiday weekend in the best way, on the trails with the IMTR crew👌. Having Cruagh as our base gives us so many options, this week alone we've hit trails in Tibradden, Masseys, Glendoo and of course Cruagh, great to be able to mix it up and keeps us on our toes. Strong running from everyone on our 4k, 7k, 10k, 17k and beyond💪. Well done gang. #irishmountaintrailrunners #IMTR #bestcrew #trainingrun #consistencyiskey",
       folder: "1st August",
